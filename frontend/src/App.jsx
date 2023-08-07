5553490d17cd3fbf054205a97e68426ea431107b
prabhaspp <indra.lochans@gmail.com>
2023-08-07 13:58:35 +0530

    added q1 part 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+function TrainList() {
+  const [trains, setTrains] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/')
+      .then(response => {
+        setTrains(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching train data:', error);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Train List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Train Name</th>
+            <th>Train Number</th>
+            <th>Departure Time</th>
+            <th>Seats Available</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trains.map(train => (
+            <tr key={train.trainNumber}>
+              <td>{train.trainName}</td>
+              <td>{train.trainNumber}</td>
+              <td>{`${train.departureTime.Hours}:${train.departureTime.Minutes}`}</td>
+              <td>{`Sleeper: ${train.seatsAvailable.sleeper}, AC: ${train.seatsAvailable.AC}`}</td>
+              <td>{`Sleeper: ${train.price.sleeper}, AC: ${train.price.AC}`}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default TrainList;
